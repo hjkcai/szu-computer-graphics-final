b6cee174053683cfbf1e78020a872ea7858b5c01
Jingkun Hua <huajingkun1214@gmail.com>
2017-12-31 14:45:46 +0800

    feat: 增大绘制区域

--- src/render.cpp
+++ src/render.cpp
@@ -70,7 +70,7 @@ void Renderer::renderShadow (const Scene *scene) {
   depthShader->use();
 
   auto V = depthViewMatrix = glm::lookAt(scene->getLightingOptions()->position, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-  auto P = depthProjectionMatrix = glm::ortho<float>(-10, 10, -10, 10, -10, 20);
+  auto P = depthProjectionMatrix = glm::ortho<float>(-25, 25, -25, 25, -25, 25);
 
   for (auto group : scene->getModelGroups()) {
     for (auto model : group->getModels()) {
