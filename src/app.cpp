21ddfb4c75d28a6a3c00095722355db9f2305268
Jingkun Hua <huajingkun1214@gmail.com>
2017-12-31 13:12:42 +0800

    feat(app): 开启抗锯齿

--- src/app.cpp
+++ src/app.cpp
@@ -15,7 +15,7 @@ Application::~Application () {
 
 void Application::createWindow (const std::string &title) {
   sf::ContextSettings settings;
-  settings.antialiasingLevel = 0;
+  settings.antialiasingLevel = 8;
   settings.majorVersion = 3;
   settings.minorVersion = 3;
   settings.depthBits = 24;
